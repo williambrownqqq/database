734dcdffae7ff36cb7a6acc14f92b49e480bc954
alexprogramest <82286522+alexprogramest@users.noreply.github.com>
2021-11-12 17:03:31 +0200

    change_20

--- shop.sql
+++ shop.sql
@@ -3,29 +3,24 @@ CREATE DATABASE SHOP;
 USE SHOP;
 CREATE TABLE Manufacturer(
     ManufacturerID INT PRIMARY KEY AUTO_INCREMENT,
-    ManufacturerID INT PRIMARY KEY,
     Brand  CHAR(20) NULL,
     Country CHAR(20) NULL
 );
 CREATE TABLE Product(
     ProductID INT PRIMARY KEY AUTO_INCREMENT,
     CategoryID INT  NOT NULL,
-    Price Float NOT NULL CHECK (Price < 0),
-    CategoryID INT NOT NULL,
     Price Float NOT NULL CHECK (Price > 0),
     Amount INT NOT NULL,
     ProductName CHAR(20) NOT NULL,
     DescribeProduct VARCHAR(2000) NULL,
-	WaitringTime INT NOT NULL,
-    Photo VARCHAR(20) NOT NULL,
+	WaitingTime INT NOT NULL,
     ManufacturerID INT NOT NULL,
     Seller CHAR(20) NOT NULL
 );
 CREATE TABLE Delivery(
-    DeliveryID INT PRIMARY KEY AUTO_INCREMENT,
+    DeliveryID INT PRIMARY KEY,
     Term CHAR(20) NOT NULL,
     ProductID INT NOT NULL,
-    Amount INT NOT NULL,
     Dealer CHAR(20) NULL
 );
 
@@ -36,8 +31,7 @@ CREATE TABLE Customer(
     Patronymic CHAR(50) NOT NULL,
     Birthday CHAR(50) NOT NULL,
     Passsword CHAR(50) UNIQUE NOT NULL,
-    Email CHAR(100) UNIQUE NOT NULL CHECK(Email !=''),
-    Email CHAR(100) UNIQUE NOT NULL CHECK(Email!=''),
+    Email CHAR(100) UNIQUE NOT NULL CHECK(Email LIKE '%@gmail.com'),
     PhoneNumber CHAR(50) UNIQUE NOT NULL CHECK(PhoneNumber!='')
     -- CHECK(Email != NULL OR PhoneNumber != NULL)
 );
@@ -52,7 +46,6 @@ CREATE TABLE Ordering(
 );
 CREATE TABLE TypeProduct(
     CategoryID INT PRIMARY KEY AUTO_INCREMENT,
-    NameOfProduct CHAR(30) NOT NULL,
     Category VARCHAR(15) NOT NULL
 );
 CREATE TABLE Basket(
@@ -64,7 +57,8 @@ CREATE TABLE Basket(
 
 -- alter table  
 ALTER TABLE Product
-ADD FOREIGN KEY (ManufacturerID) REFERENCES Manufacturer(ManufacturerID);
+ADD FOREIGN KEY (ManufacturerID) REFERENCES Manufacturer(ManufacturerID),
+ADD FOREIGN KEY (CategoryID) REFERENCES TypeProduct(CategoryID);
 ALTER TABLE Delivery
 ADD FOREIGN KEY (ProductID) REFERENCES Product(ProductID);
 ALTER TABLE Ordering
@@ -74,10 +68,34 @@ ADD FOREIGN KEY (OrderID) REFERENCES Ordering(OrderingID),
 ADD FOREIGN KEY (ProductID) REFERENCES Product(ProductID);
 
 -- insert
+INSERT INTO Manufacturer(ManufacturerID, Brand, Country)
+	VALUES (6894, "BELTGUYS", "Ukraine"),
+    (3456, "Smoby", "USA"),
+    (7878, "Lamborghini", "Italy");
 INSERT INTO Customer (Firstname, Surname, Patronymic, Birthday, Passsword, Email, PhoneNumber) 
- VALUES ("Illia", "Сидоров", "Петрович", "01.01.2003", "A>/*fE6\3QNsh{GR", "@gmail.com",  "+380234557849"),
-("lesha", "zanchenko", "dmitrievich", "01.01.2000","qwertyuiop", "mail@gmail.com", "+380445657849"),
-("lesha", "dudkin", "batkovich", "01.01.2001","qwerty123", "gmail@gmail.com", "+380444557849");
+	VALUES ("Lesha", "Dudkin", "Nikolaevich", "10.04.2003","qwerty123", "alisha@gmail.com", "+380849730945"),
+    ("Lesha", "Zanchenko", "Dmitrievich", "25.03.2002","9sdfygo9sdf", "zanlesh@gmail.com", "+380444557849"),
+    ("Jeka", "Zakharchuk", "Nikolaevich", "05.04.2001","q;sodgjlidhgl8", "zheniazk@gmail.com", "+3802342343423");
+INSERT INTO TypeProduct(CategoryID, Category)
+	VALUES (234566, "Stationery"),
+    (345890, "Toy"),
+    (012346, "Car");
+INSERT INTO Product(ProductID, CategoryID, Price, Amount, ProductName, DescribeProduct, WaitingTime, ManufacturerID, Seller) 
+	VALUES (198,012346, 33.4, 150,"Folder", "Gray folder for a gray job",5, 7878 , "Serozha"),
+    (234,345890, 10.50, 60,"Ball", "Small ball for children",10, 3456 , "Sashka"),
+    (543,012346, 111.4, 5,"Lamborgini", "luxury car",60, 7878 , "Evgenii");
+INSERT INTO Ordering(CustomerID, Term, TotalSum, TypeDelivery, ADDresss, PaymentType)
+	VALUES (1, 10, 100, "NovaPoshta", "kyiv", "cash"),
+    (2, 15, 60, "NovaPoshta", "kyiv", "cash"),
+    (3, 18, 200, "UkrPoshta", "kyiv", "cash");
+INSERT INTO Delivery(DeliveryID, Term, ProductID, Dealer)
+	VALUES (243, "term", 198, "GMC"),
+    (898, "term", 234, "GMC"),
+    (777, "term", 543, "GMC");
+INSERT INTO Basket(OrderID, ProductID, Amount)
+	VALUES (1, 198, 1),
+    (2, 234, 1),
+    (3, 543, 1);
 
 -- update
 UPDATE Customer
@@ -90,7 +108,6 @@ DO SLEEP(3);
 DELETE FROM Customer 
 WHERE Firstname = "Illia";
 SELECT * FROM Customer;
-
 
 DROP DATABASE IF EXISTS SHOP;
 
