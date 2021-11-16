fd6fb5dc6b5dd8f4b624d068ebe7ec896827c50b
Alex <iamalexzanchenko@gmail.com>
2021-11-16 11:23:36 +0200

    InnerJoin update

--- shop.sql
+++ shop.sql
@@ -110,12 +110,21 @@ WHERE Firstname = "Illia";
 SELECT * FROM Customer;
 
 SELECT * FROM Product WHERE Seller = "Serozha" AND Amount < 100;
-DO SLEEP(3);
+DO SLEEP(1);
 SELECT ProductName, DescribeProduct FROM Product WHERE ProductName Like "Ball%" ;
-DO SLEEP(3);
+DO SLEEP(1);
 SELECT * FROM Product
 ORDER BY Price;
+DO SLEEP(1);
+
+SELECT DISTINCT * FROM Product;
+SELECT * FROM Product LIMIT 2;
 DO SLEEP(3);
+
+SELECT *
+FROM Product
+INNER JOIN Basket
+ON Product.ProductID = Basket.ProductID;
 
 DROP DATABASE IF EXISTS SHOP;
 
