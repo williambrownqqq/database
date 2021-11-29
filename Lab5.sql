6a4c6e57e98e08cf9537b006fe5f24935682eb2e
alexprogramest <82286522+alexprogramest@users.noreply.github.com>
2021-11-29 14:12:33 +0200

    Answers and update of comments

--- Lab5.sql
+++ Lab5.sql
@@ -1,10 +1,14 @@
--- All agregate functions without Group by
+-- Агрегатные функции без GROUP BY
 SELECT  COUNT(ProductName) AS Количество_продуктов, MAX(Amount) AS минимум, MIN(Amount) AS максимум, AVG(Amount) AS среднее, SUM(Amount) AS сумма FROM Product;
 DO SLEEP(4);
 
--- All agregate functions with Group by 
+-- Агрегатные функции совместно с GROUP BY
 SELECT Firstname, COUNT(*) FROM Customer
 GROUP BY Firstname;
+
+-- Вывод после двух примеров с / без GROUP BY: Если команда отсутсвует, то (грубо говоря) вся таблица и есть сплошная группа
+-- Иными словами, все агрегатные функции будут применяться к каждому ряду индивидуально 
+
 
 DO SLEEP(3);
 SELECT Customer.CustomerID, Customer.Firstname, Ordering.TotalSum FROM Customer, Ordering
@@ -15,14 +19,28 @@ DO SLEEP(2);
 SELECT ProductName, COUNT(ProductName), MAX(Amount), MIN(Amount), AVG(Amount), SUM(Amount) AS Сумма_цен FROM Product
 GROUP BY ProductName;
 
--- Usage of Having
+-- Использование Having
+-- Разница между Having и Where: при группировании Having работает с каждой построенной группой и выводит ряд если группа соответсвует условиям (результат равен true)
+-- Если команда GROUP BY отсутсвует, обе выше упомянутые команды работают одинаково (каждый ряд как группа считаются как для GROUP BY)
 DO SLEEP(4);
 SELECT  Country, COUNT(*) AS Производитель
 FROM Manufacturer
 GROUP BY Country
 Having COUNT(*) > 1;
 
-DO SLEEP(2);
+
+-- Разница между DISTINCT и Group by (окромя того что второе еще и групирует) заключается в том, 
+-- что первое уберает дубликаты касательно того что мы выбрали: если после DISTINCT использовать *, 
+-- то будут убраны только сплошные дубликаты (тоесть все поля у них должны быть одинаковые)
+DO SLEEP(5);
+SELECT  DISTINCT *
+FROM Customer;
+
+DO SLEEP(5);
+SELECT  DISTINCT Firstname
+FROM Customer;
+
+DO SLEEP(5);
 SELECT  *
 FROM Customer 
 GROUP BY Firstname
