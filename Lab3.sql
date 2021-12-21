fb9ce0e25bb61742adafb6c5240b39a534d6fdda
alexprogramest <82286522+alexprogramest@users.noreply.github.com>
2021-12-21 12:11:04 +0200

    commit

---
+++ Lab3.sql
@@ -0,0 +1,43 @@
+-- insert
+INSERT INTO Manufacturer(ManufacturerID, Brand, Country)
+	VALUES (6894, "BELTGUYS", "Ukraine"),
+    (3456, "Smoby", "USA"),
+    (7878, "Lamborghini", "USA");
+INSERT INTO Customer (Firstname, Surname, Patronymic, Birthday, Passsword, Email, PhoneNumber) 
+	VALUES ("Lesha", "Dudkin", "Nikolaevich", "10.04.2003","qwerty123", "alisha@gmail.com", "+380849730945"),
+    ("Lesha", "Zanchenko", "Dmitrievich", "25.03.2002","9sdfygo9sdf", "zanlesh@gmail.com", "+380444557849"),
+    ("Jeka", "Zakharchuk", "Nikolaevich", "05.04.2001","q;sodgdfggl8", "zheniazk@gmail.com", "+3802342343423"),
+     ("Andrey", "NeDudkin", "Nikolaevich", "05.04.2023","q;sodsdfhhgl8", "andrey@gmail.com", "+38234523"),
+      ("NeAndrey", "NeZanchenko", "Nikolaevich", "05.04.1900","q;sodadsfidhgl8", "neandreyk@gmail.com", "+38568956783423");
+INSERT INTO TypeProduct(CategoryID, Category)
+	VALUES (234566, "Stationery"),
+    (345890, "Toy"),
+    (012346, "Car");
+INSERT INTO Product(ProductID, CategoryID, Price, Amount, ProductName, DescribeProduct, WaitingTime, ManufacturerID, Seller) 
+	VALUES (198,345890, 33.4, 150,"Ball", "Ball for Basketball",5, 7878 , "Serozha"),
+    (234,345890, 1011.50, 60,"Ball", "Ball for football",10, 3456 , "Serozha"),
+    (543,012346, 111.4, 5,"Lamborgini", "luxury car",60, 7878 , "Serozha");
+INSERT INTO Ordering(CustomerID, Term, TotalSum, TypeDelivery, ADDresss, PaymentType)
+	VALUES (1, 10, 100, "NovaPoshta", "kyiv", "cash"),
+    (2, 15, 60, "NovaPoshta", "kyiv", "cash"),
+    (3, 18, 200, "UkrPoshta", "kyiv", "cash");
+INSERT INTO Delivery(DeliveryID, Term, ProductID, Dealer)
+	VALUES (243, "term", 198, "GMC"),
+    (898, "term", 234, "GMC"),
+    (777, "term", 543, "GMC");
+INSERT INTO Basket(OrderID, ProductID, Amount)
+	VALUES (1, 198, 1),
+    (2, 234, 1),
+    (3, 543, 1);
+
+-- update
+UPDATE Customer
+  SET Email = CONCAT(Firstname, Email)
+    WHERE Customerid < 3;
+SELECT * FROM Customer;
+
+-- delete
+DO SLEEP(1);
+DELETE FROM Customer 
+WHERE Firstname = "Illia";
+SELECT * FROM Customer;
---
+++ Lab8.sql
@@ -0,0 +1,48 @@
+SELECT ProductName, DescribeProduct, 
+(SELECT ManufacturerID FROM Manufacturer
+WHERE Manufacturer.ManufacturerID = Product.ManufacturerID)
+As ManID
+FROM Product;
+
+SELECT ProductName,
+(SELECT Brand FROM Manufacturer
+WHERE Manufacturer.ManufacturerID = Product.ManufacturerID) AS brand
+FROM Product;
+
+
+-- --- 2
+
+SELECT ProductName,
+    (SELECT Amount FROM Basket WHERE Basket.ProductID = Product.ProductID) AS Amount
+FROM Product
+WHERE Product.ProductID > 2;
+
+
+ -- 3
+ SELECT * FROM Basket
+ WHERE (Amount < ALL(SELECT Amount FROM Product WHERE  ManufacturerID = "7878" ) AND ProductID = "198");
+ -- Выведем колличество тех товаров из корзины где колличество товаровов из корзины больше колличества товаров у производителя 7878
+
+ SELECT * FROM Product
+ WHERE Price < ANY(SELECT MAX(PRICE) FROM Product);
+
+-- 4
+
+DELETE FROM Delivery
+ WHERE ProductID = SOME(SELECT ProductID 
+						   FROM Product
+						  WHERE CategoryID = (SELECT CategoryID FROM TypeProduct WHERE Category = "Toy"));
+                          
+SELECT * FROM Delivery;
+
+INSERT INTO Delivery(DeliveryID, Term, ProductID, Dealer) 
+    VALUES(747, "termin", 234, "ZO Ball");
+
+                          
+UPDATE Product
+   SET Price = Price + 100  
+ WHERE CategoryID = (SELECT CategoryID 
+					FROM TypeProduct
+					WHERE Category = "Car");
+                    
+SELECT * FROM Product;
